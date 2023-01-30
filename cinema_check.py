e0eb341d7e242351e204632bad1b7e43b1c50501
Димитър Светлозаров <88004378+DimitarSarafski@users.noreply.github.com>
2023-01-30 15:58:42 +0200

    Delete cinema_check.py

--- cinema_check.py
+++
@@ -1,9 +0,0 @@
-day_of_the_week = input()
-cinema_ticket = 0
-if day_of_the_week == "Monday" or day_of_the_week == "Tuesday" or day_of_the_week == "Friday":
-    cinema_ticket = 12
-elif day_of_the_week == "Wednesday" or day_of_the_week == "Thursday":
-    cinema_ticket = 14
-elif day_of_the_week == "Saturday" or day_of_the_week == "Sunday":
-    cinema_ticket = 16
-print(cinema_ticket)
